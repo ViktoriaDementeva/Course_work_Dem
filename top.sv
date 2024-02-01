a269754353b163405d0017eaa1300f0e9f0302dc
ViktoriaDementeva <viktoriadementeva99@gmail.com>
2024-02-02 00:08:35 +0300

    Controllers added

---
+++ top.sv
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.02.2024 15:29:31
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+input logic clk,
+input logic rst,
+(* mark_debug = "true" *)input logic echo,
+output logic trig,  
+output logic stb,
+output logic dio,
+output logic clk_kHz
+    );
+logic [11:0] dist_from_sensor;
+logic [3:0] dist_from_bcd1;
+logic [3:0] dist_from_bcd2;
+logic [3:0] dist_from_bcd3;
+(* mark_debug = "true" *)logic [7:0] data1_to_TM;
+(* mark_debug = "true" *)logic [7:0] data2_to_TM;
+(* mark_debug = "true" *) logic [7:0] data3_to_TM;
+logic synch, synch2;
+ 
+    HC_SR04 hcsr04(.clk(clk), .rst(rst), .echo(echo), .trig(trig), .distance1(dist_from_sensor), .synch(synch));
+    BCD bcd (.clk(clk), .rst(rst), .synch(synch), .bin_in(dist_from_sensor), .dec_out1(dist_from_bcd1), .dec_out2(dist_from_bcd2), .dec_out3(dist_from_bcd3));
+    Data_to_TM1638 data1(.clk(clk), .in(dist_from_bcd1), .out(data1_to_TM));
+    Data_to_TM1638 data2(.clk(clk), .in(dist_from_bcd2), .out(data2_to_TM));
+    Data_to_TM1638 data3(.clk(clk), .in(dist_from_bcd3), .out(data3_to_TM));
+    
+    always_ff@(posedge clk)
+    if (data3_to_TM !== 8'd252)
+    synch2 <= 1'd1;
+    else 
+    synch2 <= 1'd0;
+    
+    TM1638 tm1638(.clk(clk), .synch2(synch2), .stb(stb), .rst(rst), .data1(data1_to_TM), .data2(data2_to_TM), .data3(data3_to_TM), .dio(dio), .clk_kHz(clk_kHz));
+endmodule
---
+++ top_TB.sv
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.02.2024 15:36:30
+// Design Name: 
+// Module Name: top_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_TB;
+//inputs
+logic clk, rst;
+logic [11:0] bin_in;
+//outputs
+logic i;
+ logic [3:0] dec_out1;
+ logic [3:0] dec_out2;
+ logic [3:0] dec_out3;
+ logic echo, trig, stb, dio, clk_kHz;
+ logic [11:0] dist_from_sensor;
+ logic [3:0] dist_from_bcd1;
+ logic [3:0] dist_from_bcd2;
+ logic [3:0] dist_from_bcd3;
+ logic [7:0] data1_to_TM;
+ logic [7:0] data2_to_TM;
+ logic [7:0] data3_to_TM;
+ 
+//BCD uut (.clk(clk), .rst(rst), .bin_in(bin_in), .dec_out1(dec_out1), .dec_out2(dec_out2), .dec_out3(dec_out3));
+top uut (.clk(clk), .rst(rst), .echo(echo), .trig(trig), .stb(stb), .dio(dio), .clk_kHz(clk_kHz));
+
+//initialize inputs
+initial 
+forever 
+begin
+#5 clk=~clk;
+end
+
+initial
+begin
+clk = 1'd0;
+rst = 1'd0;
+echo = 1'd0;
+#5 rst = 1'd1;
+#5 rst = 1'd0;
+#200 echo = 1'd1;
+#2000000 echo = 1'd0;
+end
+endmodule
